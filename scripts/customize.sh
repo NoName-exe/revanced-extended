b6a0b19163af631a266a0c6a3ef9c70f474f1cfa
kevinr99089 <93716347+kevinr99089@users.noreply.github.com>
2023-04-30 14:31:06 +0200

    add option include-stock

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -15,7 +15,7 @@ fi
 set_perm_recursive $MODPATH/bin 0 0 0755 0777
 
 nsenter -t1 -m -- grep __PKGNAME /proc/mounts | while read -r line; do
-	ui_print "- Un-mount"
+	ui_print "* Un-mount"
 	mp=${line#* }
 	mp=${mp%% *}
 	nsenter -t1 -m -- umount -l "${mp%%\\*}"
@@ -27,17 +27,33 @@ if BASEPATH=$(pm path __PKGNAME); then
 	BASEPATH=${BASEPATH##*:}
 	BASEPATH=${BASEPATH%/*}
 	if [ ${BASEPATH:1:6} = system ]; then
-		ui_print "- __PKGNAME is a system app"
+		ui_print "* __PKGNAME is a system app"
 	elif [ ! -d ${BASEPATH}/lib ]; then
-		ui_print "- Invalid installation found. Uninstalling..."
+		ui_print "* Invalid installation found. Uninstalling..."
 		pm uninstall -k --user 0 __PKGNAME
+	elif [ ! -f $MODPATH/__PKGNAME.apk ]; then
+		ui_print "* Stock __PKGNAME APK was not found"
+		VERSION=$(dumpsys package __PKGNAME | grep -m1 versionName)
+		VERSION="${VERSION#*=}"
+		if [ "$VERSION" = __PKGVER ] || [ -z "$VERSION" ]; then
+			ui_print "* Skipping stock installation"
+			INS=false
+		else
+			abort "ERROR: Version mismatch
+			installed: $VERSION
+			module:    __PKGVER
+			"
+		fi
 	elif cmpr $BASEPATH/base.apk $MODPATH/__PKGNAME.apk; then
-		ui_print "- __PKGNAME is up-to-date"
+		ui_print "* __PKGNAME is up-to-date"
 		INS=false
 	fi
 fi
 if [ $INS = true ]; then
-	ui_print "- Updating __PKGNAME (v__PKGVER)"
+	if [ ! -f $MODPATH/__PKGNAME.apk ]; then
+		abort "ERROR: Stock __PKGNAME apk was not found"
+	fi
+	ui_print "* Updating __PKGNAME to __PKGVER"
 	settings put global verifier_verify_adb_installs 0
 	SZ=$(stat -c "%s" $MODPATH/__PKGNAME.apk)
 	if ! SES=$(pm install-create --user 0 -i com.android.vending -r -d -S "$SZ" 2>&1); then
@@ -65,7 +81,7 @@ if [ $INS = true ]; then
 fi
 BASEPATHLIB=${BASEPATH}/lib/${ARCH}
 if [ -z "$(ls -A1 ${BASEPATHLIB})" ]; then
-	ui_print "- Extracting native libs"
+	ui_print "* Extracting native libs"
 	mkdir -p $BASEPATHLIB
 	if ! op=$(unzip -j $MODPATH/__EXTRCT lib/${ARCH_LIB}/* -d ${BASEPATHLIB} 2>&1); then
 		ui_print "ERROR: extracting native libs failed"
@@ -73,10 +89,10 @@ if [ -z "$(ls -A1 ${BASEPATHLIB})" ]; then
 	fi
 	set_perm_recursive ${BASEPATH}/lib 1000 1000 755 755 u:object_r:apk_data_file:s0
 fi
-ui_print "- Setting Permissions"
+ui_print "* Setting Permissions"
 set_perm $MODPATH/base.apk 1000 1000 644 u:object_r:apk_data_file:s0
 
-ui_print "- Mounting __PKGNAME"
+ui_print "* Mounting __PKGNAME"
 mkdir -p $NVBASE/rvhc
 RVPATH=$NVBASE/rvhc/__PKGNAME_rv.apk
 mv -f $MODPATH/base.apk $RVPATH
@@ -86,12 +102,16 @@ if ! op=$(nsenter -t1 -m -- mount -o bind $RVPATH $BASEPATH/base.apk 2>&1); then
 	ui_print "$op"
 fi
 am force-stop __PKGNAME
-ui_print "- Optimizing __PKGNAME"
+ui_print "* Optimizing __PKGNAME"
 nohup cmd package compile --reset __PKGNAME >/dev/null 2>&1 &
 
-ui_print "- Cleanup"
+ui_print "* Cleanup"
 rm -rf $MODPATH/bin $MODPATH/__PKGNAME.apk
 
 for s in "uninstall.sh" "service.sh"; do
 	sed -i "2 i\NVBASE=${NVBASE}" $MODPATH/$s
 done
+
+ui_print "* Done"
+ui_print "  by j-hc (github.com/j-hc)"
+ui_print " "
