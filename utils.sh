8c5d90a3e3a66c95e45e4b7ee525fe1a31e47296
vuong2023 <124447749+vuong2023@users.noreply.github.com>
2023-07-30 16:33:48 +0700

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -508,7 +508,7 @@ module_prop() {
 name=${2}
 version=v${3}
 versionCode=${NEXT_VER_CODE}
-author=NoName-exe
+author=vuong2023
 description=${4}" >"${6}/module.prop"
 
 	if [ "$ENABLE_MAGISK_UPDATE" = true ]; then echo "updateJson=${5}" >>"${6}/module.prop"; fi
