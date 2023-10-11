99604eb6b163bd5ac7773d6e79e7bef33dcf2b58
NoName-exe <52629189+NoName-exe@users.noreply.github.com>
2023-10-11 16:49:43 +0530

    oops

--- build.sh
+++ build.sh
@@ -183,7 +183,7 @@ if [ "$youtube_mode" != module ] || [ "$music_mode" != module ]; then
 	log "\nInstall [Vanced MicroG](https://github.com/inotia00/VancedMicroG/releases) or [mMicroG](https://github.com/inotia00/mMicroG/releases) to be able to use non-root YouTube or YouTube-Music."
 fi
 if [ "$youtube_mode" != apk ] || [ "$music_mode" != apk ]; then
-	log "Use [mindetach](https://github.com/j-hc/mindetach-magisk) or [zygisk-detach](https://github.com/j-hc/zygisk-detach) to block Play Store from updating YouTube and YouTube-Music.
+	log "Use [mindetach](https://github.com/j-hc/mindetach-magisk) or [zygisk-detach](https://github.com/j-hc/zygisk-detach) to block Play Store from updating YouTube and YouTube-Music."
 fi
 log "\n[Main Repo](https://github.com/NoName-exe/revanced-extended)"
 log "\nChangelog:"
